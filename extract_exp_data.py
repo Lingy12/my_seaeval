63bcd24f848d9bb5fc8e7cb483ed9f263a41234c
Lin Geyu <lingy@asp2a-login-astar02.head.cm.asp2a.nscc.sg>
2023-11-21 16:07:17 +0800

    update script for eval

--- extract_exp_data.py
+++ extract_exp_data.py
@@ -2,7 +2,9 @@ import os
 import fire
 import json
 from pathlib import Path
-def extract_data(exp_root):
+import pandas as pd
+
+def extract_data(exp_root, data_name):
     results_data = []
 
     # Loop through each subdirectory in the given root directory
@@ -22,10 +24,13 @@ def extract_data(exp_root):
                                 data = json.load(json_file)
                                 # print(os.path.pardir(subdir))
                                 model_path = Path(subdir)
-                                print(model_path.parent.absolute())
+                                # print(model_path.parent.absolute())
         
                                 extracted_data = {
-                                    "name": f"{os.path.basename(model_path.parent.absolute())}_{os.path.basename(subdir).split('-')[1]}_{os.path.basename(file_path)[:-5]}",
+                                    "name": f"{os.path.basename(model_path.parent.absolute())}",
+                                    "step": os.path.basename(subdir).split('-')[1],
+                                    "test_data": os.path.basename(file_path)[:-5],
+                                    "test_mode": test_folder,
                                     'total_accuracy': data.get('Accuracy', None),
                                     'consistency_3': data.get('Consistency', {}).get('consistency_3', None),
                                     'AC3_3': data.get('AC3', {}).get('AC3_3', None),
@@ -36,8 +41,10 @@ def extract_data(exp_root):
                                     extracted_data[key] = value
                                 results_data.append(extracted_data)
 
-    print(results_data)
-    return results_data
+    # print(results_data)
+    data = pd.DataFrame.from_dict(results_data)
+    data.sort_values(by=["name", "step"])
+    data.to_excel(f'summary/{data_name}.xlsx')
 
 if __name__ == "__main__":
     fire.Fire(extract_data)
--- scripts/automatic_checking.sh
+++ scripts/automatic_checking.sh
@@ -1,44 +1,36 @@
 LISTEN_FOLDER=$1
 
 
-while :
+echo "Checking new models missing evaluation... for $LISTEN_FOLDER"
+echo time is `date`
+
+for dir in $LISTEN_FOLDER/*; 
 do 
-    echo "Checking new models missing evaluation... for $LISTEN_FOLDER every 60 minutes"
-    echo time is `date`
+    BASENAME=`basename "$dir"` 
 
-    for dir in $LISTEN_FOLDER/*; 
-    do 
-        BASENAME=`basename "$dir"` 
+    if [[ $BASENAME == checkpoint-* && $BASENAME != *results ]];
+    then
+        
+        echo $BASENAME            
+        FULLPATH=$LISTEN_FOLDER/$BASENAME-results
+        echo 'Checking' $FULLPATH
 
-        if [[ $BASENAME == checkpoint-* && $BASENAME != *results ]];
+        if [ ! -d "$FULLPATH" ]; 
         then
-            
-            echo $BASENAME            
-            FULLPATH=$LISTEN_FOLDER/$BASENAME-results
-            echo 'Checking' $FULLPATH
 
-            if [ ! -d "$FULLPATH" ]; 
-            then
+            echo "=  =  =  =  =  =  =  =  =  =  =  =  =  =  =  ="
+            echo "$FULLPATH does not exist."
+            mkdir -p $FULLPATH
+            MODEL_NAME=$(basename $LISTEN_FOLDER)
+            # Do Conversion and Evaluation
+            echo "Converting and evaluating $LISTEN_FOLDER/$BASENAME"
+            mkdir -p log
+            qsub -v "MODEL_INDEX=$BASENAME,LISTEN_FOLDER=$LISTEN_FOLDER" -o "log/${MODEL_NAME}_${BASENAME}.log" -N "${MODEL_NAME}_${BASENAME}" scripts/convert_and_evaluate.sh
 
-                echo "=  =  =  =  =  =  =  =  =  =  =  =  =  =  =  ="
-                echo "$FULLPATH does not exist."
-                mkdir -p $FULLPATH
-                MODEL_NAME=$(basename $LISTEN_FOLDER)
-                # Do Conversion and Evaluation
-                echo "Converting and evaluating $LISTEN_FOLDER/$BASENAME"
-                mkdir -p log
-                qsub -v "MODEL_INDEX=$BASENAME,LISTEN_FOLDER=$LISTEN_FOLDER" -o "log/${MODEL_NAME}_${BASENAME}.log" -N "${MODEL_NAME}_${BASENAME}" scripts/convert_and_evaluate.sh
-
-
-            fi
 
         fi
 
-    done
-
-    echo time is `date`
-    echo 'Sleeping for 60 minutes'
-    sleep 3600
+    fi
 
 done
-
+echo "$LISTEN_FOLDER finished"
