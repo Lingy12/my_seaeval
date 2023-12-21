8d04ced2da8d40f3bedb67649654ffbfab546cd8
lingy <lingy@i2r.a-star.edu.sg>
2023-12-21 19:35:13 +0800

    update script to convert lora

--- convert_lora.py
+++ convert_lora.py
@@ -1,17 +1,26 @@
 import fire
-from transformers import AutoModel
+from transformers import AutoModel, AutoModelForCausalLM
+from peft.peft_model import PeftModel
 import torch
 import glob
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = ""
 def convert_checkpoint(checkpoint_folder, base_model, destination):
-    parameter_file = glob.glob(os.path.join(checkpoint_folder, 'global_step*', 'mp_rank_00_model_states.pt'))
-    model_state_dict = torch.load(parameter_file[0], map_location=torch.device('cpu'))
+    adapter = os.path.join(checkpoint_folder, 'pt_lora_model')
 
-    print('Loading parameter file = {}'.format(parameter_file[0]))
-    model = AutoModel.from_pretrained(base_model, state_dict = model_state_dict, device_map='auto')
+    # print('Loading parameter file = {}'.format(adapter))
+    model = AutoModelForCausalLM.from_pretrained(base_model, device_map='auto')
 
-    model.save_pretrained(destination, max_shard_size='20GB')
+    # model = PeftModel.from_pretrained(adapter, "outputs")
+    # model.load_adapter(adapter)
+    model.resize_token_embeddings(32001) # since <pad> is used
+    # model.load_adapter(adapter)
+    
+    model = PeftModel.from_pretrained(model, adapter)
+    model = model.merge_and_unload()
+    # model.save_pretrained(destination, max_shard_size='20GB')
+    # model = model.merge_and_upload()
+    model.save_pretrained(destination)
 
 if __name__ == "__main__":
     fire.Fire(convert_checkpoint)
